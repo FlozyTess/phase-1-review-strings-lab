26360ce654aa4e79a5ff6c6a34989c0a8993d9c2
FlozyTess <cecilrency23@gmail.com>
2024-11-30 15:22:43 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-const currentUser = 'Tess'; // Write your code in this file!
-const welcomeMessage = 'Welcome to Flatbook';
+const currentUser = 'Grace Hopper'; // Write your code in this file!
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting =`Welcome, ${currentUser.slice(0,1)}!`;
